0f86cf6e85499968105168c8e04c877b3f69ef2e
hassanali1996h <HUGNVF@GMAIL.COM>
2025-09-01 05:58:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-
-const cacheName = "quran-analysis-v1";
+// /digital-analysis4/sw.js
+const cacheName = "quran-analysis-v2";
 const assets = [
   "/digital-analysis4/",
   "/digital-analysis4/index.html",
@@ -7,21 +7,24 @@ const assets = [
   "/digital-analysis4/sw.js",
   "/digital-analysis4/icon-192.png",
   "/digital-analysis4/icon-512.png",
-  "/digital-analysis4/cmudict_full.js"
+  "/digital-analysis4/surahs_full.json"
+  // "/digital-analysis4/cmudict_full.js" // أضِفها فقط إذا الملف موجود فعلاً
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
